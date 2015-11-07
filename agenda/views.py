b55aa0c81412c6d9e61089fc080216a7185c58d2
Mario <yo@hotmail.com>
2015-11-07 17:06:17 -0600

    Se agregaron plantillas html

--- agenda/views.py
+++ agenda/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Agendar
 def listar_publicaciones(request):
-    return render (request, 'agenda/listar_publicaciones.html',{})
+    actividades = Agendar.objects.filter(fecha_actividad__lte=timezone.now()).order_by('fecha_actividad')
+    return render(request, 'agenda/listar_publicaciones.html', {'actividades': actividades})
+
 
 # Create your views here.
